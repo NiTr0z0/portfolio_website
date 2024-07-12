0756191df660737e4ac765e10c0238baa2a9f728
NiTr0z0 <162600608+NiTr0z0@users.noreply.github.com>
2024-07-12 13:25:15 +0530

    mobile view fix

--- src/pages/CP.tsx
+++ src/pages/CP.tsx
@@ -1,7 +1,6 @@
 import "../components/Hero.css";
 import "./CP.css";
 import Navbar from "../components/Navbar";
-import codeChefLogo from "../assets/icons8-codechef.svg";
 import leedCodeLogo from "../assets/leetcode.svg";
 import cfLogo from "../assets/code-forces.svg";
 import Footer from "../components/footer";
@@ -12,12 +11,10 @@ const CP = () => {
     <div>
       <Navbar items={navbarItems} logo="Abhinav Khot" />
       <div className="page-container">
-        <h3>You can find me on the following platforms:</h3>
+        <h3>
+          <center>You can find me on:</center>
+        </h3>
         <div className="links-container">
-          <a href="https://codechef.com/users/nitrozo" className="icon">
-            <img src={codeChefLogo} alt="codechef"></img>
-            <figcaption font-family="arial">codechef</figcaption>
-          </a>
           <a href="https://leetcode.com/u/skabhinav018/" className="icon">
             <img className="icon" src={leedCodeLogo} alt="leetcode"></img>
             <figcaption font-family="arial">LeetCode</figcaption>
